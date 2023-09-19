198b097e04669f928f53ad397b99af9b8ff81987
bappyasif <bappyasif@hotmail.com>
2023-09-19 21:56:08 +0600

    trying out new library
    * added a few big calender library basic uses
    * now wil be trying out how to actively make changes to them

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { HomeHero } from "@/components/forHome/HomeHero";
 import { RandomizeSelection } from "@/components/forHome/RandomizeSelection";
 import { RecentlyViewedMealsScroller } from "@/components/forHome/RecentlyViewedMealsScroller";
 import { VerticalCarousel } from "@/components/forHome/VerticalCarousel";
+import { Scheduler } from "@/components/forUtilities/bigCalender/tryouts-II/Scheduler";
+// import { Scheduler } from "@/components/forUtilities/bigCalender/Scheduler";
 import { DragAndDrop } from "@/components/forUtilities/dragAndDrop/DragAndDrop";
 import { Button } from "@/components/ui/button";
 
@@ -12,6 +14,7 @@ export default function Home() {
     <div className="h-[100vh] bg-primary">
       <h1>Home Page</h1>
       <HomeHero />
+      <Scheduler />
       <DragAndDrop />
       <RandomizeSelection />
       {/* <Button variant={"ghost"} className="bg-primary-content hover:bg-primary-foreground">Button Here</Button> */}
---
+++ src/components/forUtilities/bigCalender/tryouts-II/Scheduler.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from 'react'
+import { MostBasic } from './MostBasic'
+
+export const Scheduler = () => {
+  return (
+    <div>
+        Scheduler
+        <MostBasic />
+    </div>
+  )
+}
