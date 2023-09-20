3b5d2a1f0e2067949a83dbd5f5bc7522db114f74
bappyasif <bappyasif@hotmail.com>
2023-09-20 19:50:09 +0600

    added functionality
    * drag and drop calendar is in view
    * event is moveable and resizeable

--- src/components/forUtilities/bigCalender/tryouts-II/Scheduler.tsx
+++ src/components/forUtilities/bigCalender/tryouts-II/Scheduler.tsx
@@ -3,11 +3,13 @@
 import React from 'react'
 import { MostBasic } from './MostBasic'
 import { WithoutDnd } from './WithoutDnd'
+import { WithDnd } from './WithDnd'
 
 export const Scheduler = () => {
   return (
     <div>
         Scheduler
+        <WithDnd />
         <WithoutDnd />
         <MostBasic />
     </div>
