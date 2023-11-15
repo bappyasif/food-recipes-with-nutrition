e7c1be62e56acc72cb2ea9fbdaab26ee1477baf1
bappyasif <bappyasif@hotmail.com>
2023-11-15 19:44:33 +0600

    added functionality
    * added prisma adapter based authentication
    * added authentication related schema and updated it into mongodb
    * added email based authentication email login feature
    * configured nextAuthOption object with EmailProvider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,22 @@
 import { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import EmailProvider from "next-auth/providers/email"
+import {PrismaAdapter} from "@auth/prisma-adapter"
+import prisma from "@/utils/prismaClientHandler"
 
 export const nextAuthOptions:NextAuthOptions = {
     secret: process.env.NEXTAUTH_SECRET,
-    // nextUrl: process.env.NODE_ENV === "development" ? "http://localhost:3000" : process.env.NEXTAUTH_URL,
+    
+    adapter: PrismaAdapter(prisma),
+
     providers: [
         GoogleProvider({
             clientId: (process.env.GOOGLE_CLIENT_ID) as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+        }),
+        EmailProvider({
+            server: process.env.EMAIL_SERVER,
+            from: process.env.EMAIL_FROM
         })
     ]
 }
