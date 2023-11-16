530175eb78213291017ac5f5d44f49155d8eee78
bappyasif <bappyasif@hotmail.com>
2023-11-16 20:18:13 +0600

    added features
    * login with credentials now also authorizing session token for users
    * added a new page for user signup with credentials
    * added a new api route handler for issuing new user signup process
    * added a new db request handler for carrying out signup process from page
    * adjusted credential providers codes to only account for email,password

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,7 +4,7 @@ import EmailProvider from "next-auth/providers/email"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from "@/utils/prismaClientHandler"
 import CredentialsProvider from "next-auth/providers/credentials"
-import {compare, hash} from "bcrypt"
+import {compare} from "bcrypt"
 
 export const nextAuthOptions: NextAuthOptions = {
     secret: process.env.NEXTAUTH_SECRET,
@@ -24,7 +24,6 @@ export const nextAuthOptions: NextAuthOptions = {
             name: "Login With Credentials",
 
             credentials: {
-                username: { label: "Your Username", type: "text", placeholder: "jsmith" },
                 email: { label: "Your Email", type: "email", placeholder: "jsmith@wc.yo" },
                 password: { label: "Your Password", type: "password" }
             },
@@ -34,55 +33,22 @@ export const nextAuthOptions: NextAuthOptions = {
                 const userFound = await prisma.creduser.findUnique({
                     where: {
                         email: credentials?.email,
-                        // password: credentials?.password
                     }
                 })
 
                 const isValid = await compare(credentials?.password!, userFound?.password!)
 
-                console.log(isValid, "isValid!!")
-
                 if (isValid) {
-                // if (userFound?.email) {
                     return userFound
                 } else {
-                    const hashedPassword = await hash(credentials?.password!, 11)
-                    console.log(hashedPassword, "hashed P{asswor!!")
-
-                    if(hashedPassword) {
-                        const newUser = await prisma.creduser.create({
-                            data: {
-                                email: credentials?.email!,
-                                // password: credentials.password,
-                                // password: await hash(credentials.password, 11),
-                                password: hashedPassword,
-                                userName: credentials?.username!
-                            }
-                        })
-
-                        return newUser
-
-                    } else {
-                        throw new Error('User does not exists. Please make sure you insert the correct email & password.')
-                    }
+                    throw new Error('User does not exists. Please make sure you insert the correct email & password.')
                 }
             },
         }),
     ],
-    callbacks: {
-        // redirect: async ({ url, baseUrl }) => {
-        //     return baseUrl
-        // },
-        jwt: async ({ token, user, account, profile, isNewUser }) => {
-            if (user) {
-                token.id = user.id
-            }
-            return token
-        },
-        session: async ({ session, user, token }) => {
-            token?.user && (session.user = token.user)
-
-            return session
-        }
+    debug: process.env.NODE_ENV === "development",
+    // when using with credentials, this needs to be a jwt token otherwise session wont be created for user
+    session: {
+        strategy: "jwt"
     }
 }
