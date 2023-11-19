6ec33d614d247f293c6718f3fa2eca18d50dbc0b
bappyasif <bappyasif@hotmail.com>
2023-11-20 02:09:51 +0600

    adding functionality
    * adding page traversal in popular recipes page
    * using a state variable to keep track of currently viewing page and recipes data

--- src/components/forPopularRecipes/ShowRecipes.tsx
+++ src/components/forPopularRecipes/ShowRecipes.tsx
@@ -2,7 +2,7 @@
 
 import { useAppSelector } from '@/hooks/forRedux'
 import { RecipeMealType } from '@/types'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Badge } from '../ui/badge'
 import { Card, CardContent, CardFooter, CardHeader } from '../ui/card'
 import Link from 'next/link'
@@ -12,30 +12,67 @@ import { useLocale } from 'next-intl'
 import Image from 'next/image'
 import moment from 'moment'
 import { useRouter } from 'next/navigation'
+import { Button } from '../ui/button'
 
-export const ShowRecipes = ({user}:{user: any}) => {
+type data = {
+  page: number;
+  data: RecipeMealType[];
+}
+
+export const ShowRecipes = ({ user }: { user: any }) => {
   const locale = useLocale();
-  const {push, replace} = useRouter()
-  
-  if(!user?.email) {
+
+  const { push, replace } = useRouter();
+
+  if (!user?.email) {
     // push(`${locale}/prompt-user`)
     // push(`prompt-user`)
     replace(`/${locale}/prompt-user`)
     // replace(`prompt-user`)
   }
 
-
   const recipesList = useAppSelector(state => state.recipes.list)
 
-  const renderRecipes = () => recipesList?.map(item => <RenderRecipe key={item.uri} data={item} />)
+  const [showing, setShowing] = useState<data>({page: 0, data: []})
 
-  if(!user?.email) {
+  const handleOnce = () => {
+    const firstEight = recipesList.slice(0, 8)
+    setShowing({data: firstEight, page: 1})
+  }
+
+  useEffect(() => {
+    recipesList.length && handleOnce()
+  }, [recipesList])
+
+  const handleNext = () => {
+    console.log("handle next!!")
+  }
+
+  const handlePrev = () => {
+    console.log("handle prev!!")
+  }
+
+  const renderRecipes = () => showing?.data?.map(item => <RenderRecipe key={item.uri} data={item} />)
+
+  // const renderRecipes = () => recipesList?.map(item => <RenderRecipe key={item.uri} data={item} />)
+
+  if (!user?.email) {
     return
   }
 
   return (
-    <div className='grid xxs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
-      {renderRecipes()}
+    <div className='flex flex-col gap-y-4'>
+      <div 
+        className='grid xxs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 
+        xl:grid-cols-4 gap-4'
+      >
+        {renderRecipes()}
+      </div>
+
+      <div className='flex gap-x-4 justify-center w-full'>
+        <Button onClick={handlePrev}>Prev</Button>
+        <Button onClick={handleNext}>Next</Button>
+      </div>
     </div>
   )
 }
@@ -61,17 +98,18 @@ const RenderRecipe = ({ data }: { data: Partial<RecipeMealType> }) => {
         className='xxs:w-full h-48 object-fill rounded-sm transition-all duration-700 hover:object-cover mix-blend-lighten'
         blurDataURL={url} placeholder='blur' loading='lazy'
       /> */}
+
       <img
         // src={url} 
-        src={checkIfDayOlder() ? `https://source.unsplash.com/random/200?recipe=${label}` : url} 
+        src={checkIfDayOlder() ? `https://source.unsplash.com/random/200?recipe=${label}` : url}
         alt={label!} width={width} height={height}
         className='xxs:w-full h-48 object-fill rounded-sm transition-all duration-700 hover:object-cover mix-blend-lighten'
         // blurDataURL={url} placeholder='blur' 
         loading='lazy'
       />
 
-      <CardHeader 
-        className='font-bold xxs:text-lg md:text-xl xl:text-2xl text-muted-foreground hover:text-primary' 
+      <CardHeader
+        className='font-bold xxs:text-lg md:text-xl xl:text-2xl text-muted-foreground hover:text-primary'
         title={`Recipe: ${label}, Click To View details`}
       >
         <Link href={recipeLink}>{label!?.length > 20 ? ellipsedText(label!, 19) : label}</Link>
@@ -82,7 +120,7 @@ const RenderRecipe = ({ data }: { data: Partial<RecipeMealType> }) => {
         <ReuseableBadge txt='Carbon Emission' val={co2EmissionsClass} />
         <ReuseableBadge txt='Cuisine' val={typeof cuisineType === "object" ? cuisineType[0] : cuisineType} />
       </CardContent>
-      
+
       <CardFooter>
         <Link className='w-full bg-accent text-center font-bold xxs:text-lg md:text-xl xl:text-2xl text-muted-foreground hover:text-muted hover:bg-special-foreground rounded-lg' href={`/${locale}/recipe/${extractRecipeId(uri!)}`}>See Details</Link>
       </CardFooter>
